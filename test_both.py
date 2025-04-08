cd878c62e59473a24796474cbd576df7ddd8cfb4
meng chin <apple912162@gmail.com>
2025-04-08 23:27:23 +0800

    add test file

--- test_both.py
+++ test_both.py
@@ -4,4 +4,4 @@ def test_addNum():
     assert addNum(2, 3) == 5  # 測試「2 + 3」結果是否為 5
 
 def test_minusNum():
-    assert minusNum(5, 3) == 2  # 測試「5 - 3」結果是否為 2
+    assert minusNum(5, 3) == 15  # 測試「5 - 3」結果是否為 2
